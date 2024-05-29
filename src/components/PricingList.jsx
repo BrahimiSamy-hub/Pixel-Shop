56a956a2a01fe9e8ed89b5eb8ad413b222d48d4c
Samy <BrahimiSamy123@gmail.com>
2024-05-29 14:48:04 +0100

    rules added

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -18,9 +18,9 @@ const PricingList = () => {
           <div className='w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-4 [&>h4]:last:text-color-3'>
             <h4 className='h4 mb-4'>{item.title}</h4>
 
-            {/* <p className='body-2 min-h-[4rem] mb-3 text-n-1/50'>
+            <p className='body-2 min-h-[4rem] mb-3 text-n-1/50'>
               {item.description}
-            </p> */}
+            </p>
             <small>starting from</small>
             <div className='flex items-center h-[5.5rem] mb-6'>
               <div className='text-[5.5rem] leading-none font-bold'>
